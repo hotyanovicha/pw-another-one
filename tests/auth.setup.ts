bf534c1ac419a81a9f6a317825dbcba5c5193b04
hotyanovicha <hotyanovicha@gmail.com>
2026-01-04 15:48:29 +0100

    added worker index

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -2,19 +2,24 @@ import { test as setup } from '@playwright/test';
 import fs from 'fs';
 import { PageManager } from '@/ui/pages/page-manager';
 import { getUserByIndex } from '@/utils/users';
+import config from '../playwright.config';
 
-setup('authenticate', async ({ page }, testInfo) => {
-	fs.mkdirSync('.auth', { recursive: true });
+const WORKER_COUNT = config.workers ?? 1;
 
-	const user = getUserByIndex(testInfo.workerIndex);
-	const pages = new PageManager(page);
+for (let i = 0; i < WORKER_COUNT; i++) {
+	setup(`authenticate user ${i}`, async ({ page }) => {
+		fs.mkdirSync('.auth', { recursive: true });
 
-	await page.goto('/login');
-	await pages.consentDialog.acceptIfVisible();
-	await pages.loginSignupPage.login(user.email, user.password);
-	await pages.header.isLoaded();
+		const user = getUserByIndex(i);
+		const pages = new PageManager(page);
 
-	await page.context().storageState({
-		path: `.auth/user-${testInfo.workerIndex}.json`,
+		await page.goto('/login');
+		await pages.consentDialog.acceptIfVisible();
+		await pages.loginSignupPage.login(user.email, user.password);
+		await pages.header.isLoaded();
+
+		await page.context().storageState({
+			path: `.auth/user-${i}.json`,
+		});
 	});
-});
+}
